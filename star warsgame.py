ca9313bcdbae43a4b0f73a0bc6d8825c6695bf7e
RoyMustan <139050532+RoyMustan@users.noreply.github.com>
2024-02-16 19:20:41 +0530

    Update star warsgame.py
    there are some error occuring if you face any issue inform me

--- star warsgame.py
+++ star warsgame.py
@@ -1,5 +1,3 @@
-#This is the First Part:
-
 import turtle
 import math
 import random
@@ -8,47 +6,36 @@ window = turtle.Screen()
 window.setup(width=600, height=600)
 window.title("Star Wars Game by Pankaj")
 window.bgcolor("black")
-
 window.tracer(0)
 
 vertex = ((0,15),(-15,0),(-18,5),(-18,-5),(0,0),(18,-5),(18, 5),(15, 0))
 window.register_shape("player", vertex)
 
 asVertex = ((0, 10), (5, 7), (3,3), (10,0), (7, 4), (8, -6), (0, -10), (-5, -5), (-7, -7), (-10, 0), (-5, 4), (-1, 8))
-window.register_shape("Enemy", asVertex)
-
-####################
-#This is the Second Part:
-
+window.register_shape("chattan", asVertex)
 
 class Pankaj(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
-
         self.speed(0)
         self.penup()
 
-
-def Pankaj(t1, t2):
+def calculate_angle(t1, t2):
     x1 = t1.xcor()
     y1 = t1.ycor()
     
     x2 = t2.xcor()
     y2 = t2.ycor()
     
-    taauko = math.atan2(y1 - y2, x1 - x2)
-    taauko = taauko * 180.0 / 3.14159
+    angle = math.atan2(y1 - y2, x1 - x2)
+    angle = angle * 180.0 / math.pi
     
-    return taauko
+    return angle
 
-
-player = Arnav()
+player = Pankaj()
 player.color("white")
 player.shape("player")
 player.score = 0
-
-####################
-#This is the Third Part
 
 missiles = []
 for _ in range(3):
@@ -64,35 +51,26 @@ pen = Pankaj()
 pen.color("white")
 pen.hideturtle()
 pen.goto(0, 250)
-pen.write("Score: 0", False, align = "center", font = ("Arial", 24, "normal"))
+pen.write("Score: 0", False, align="center", font=("Arial", 24, "normal"))
 
-####################
-#This is the Fourth Part
-
-
-Enemy = []
-
+chattans = []
 for _ in range(5):   
-    Enemy = Pankaj()
-    Enemy.color("brown")
-    Enemy.shape("arrow")
-
-    enemy.speed  = random.randint(2, 3)/50
-    enemy.goto(0, 0)
-    taauko = random.randint(0, 260)
+    chattan = Pankaj()
+    chattan.color("brown")
+    chattan.shape("arrow")
+    chattan.speed = random.randint(2, 3)/50
+    chattan.goto(0, 0)
+    angle = random.randint(0, 260)
     distance = random.randint(300, 400)
-    Enemy.setheading(taauko)
-    Enemy.fd(distance)
-    Enemy.setheading(Arnav (player, chattan))
-    Enemy.append(chattan)
+    chattan.setheading(angle)
+    chattan.fd(distance)
+    chattan.setheading(calculate_angle(player, chattan))
+    chattans.append(chattan)
 
-####################
-#This is the Functions for Defence Part
-
-def baanya():
+def turn_left():
     player.lt(20)
     
-def daanya():
+def turn_right():
     player.rt(20)
     
 def fire_missile():
@@ -104,41 +82,35 @@ def fire_missile():
             missile.state = "fire"
             break
 
-
 window.listen()
-window.onkey(baanya, "Left")
-window.onkey(daanya, "Right")
+window.onkey(turn_left, "Left")
+window.onkey(turn_right, "Right")
 window.onkey(fire_missile, "space")
 
-####################
-#This is the Functioning the Code Part-1
-
-sakkyo = False
-while True:
-
+game_over = False
+while not game_over:
     window.update()
     player.goto(0, 0)
     
-
     for missile in missiles:
         if missile.state == "fire":
             missile.fd(missile.speed)
         
-        if missile.xcor() > 300 or missile.xcor() < -300 or missile.ycor() > 300 or missile.ycor() < -300:
+        if abs(missile.xcor()) > 300 or abs(missile.ycor()) > 300:
             missile.hideturtle()
             missile.state = "ready"
 
-    for Enemy in Enemy:    
-        Enemy.fd(Enemy.speed)
+    for chattan in chattans:    
+        chattan.fd(chattan.speed)
         
         for missile in missiles:
-            if Enemy.distance(missile) < 20:
-                taauko = random.randint(0, 260)
+            if chattan.distance(missile) < 20:
+                angle = random.randint(0, 260)
                 distance = random.randint(600, 800)
-                Enemy.setheading(taauko)
-                Enemy.fd(distance)
-                Enemy.setheading(Arnav(player, Enemy))
-                Enemy.speed += 0.01
+                chattan.setheading(angle)
+                chattan.fd(distance)
+                chattan.setheading(calculate_angle(player, chattan))
+                chattan.speed += 0.01
                 
                 missile.goto(600, 600)
                 missile.hideturtle()
@@ -146,28 +118,26 @@ while True:
                 
                 player.score += 10
                 pen.clear()
-                pen.write("Score: {}".format(player.score), False, align = "center", font = ("Arial", 24, "normal"))
+                pen.write("Score: {}".format(player.score), False, align="center", font=("Arial", 24, "normal"))
 
-        ####################
-        #This is the Functioning the Code Part-2
-
-        if Enemy.distance(player) < 20:
-            taauko = random.randint(0, 260)
+        if chattan.distance(player) < 20:
+            angle = random.randint(0, 260)
             distance = random.randint(600, 800)
-            Enemy.setheading(taauko)
-            Enemy.fd(distance)
-            Enemy.setheading(Arnav(player, Enemy))
-            enemy.speed += 0.005
-            sakkyo = True
+            chattan.setheading(angle)
+            chattan.fd(distance)
+            chattan.setheading(calculate_angle(player, chattan))
+            chattan.speed += 0.005
+            game_over = True
             player.score -= 30
             pen.clear()
-            pen.write("Score: {}".format(player.score), False, align = "center", font = ("Arial", 24, "normal"))
-    if sakkyo == True:
-        player.hideturtle()
-        missile.hideturtle()
-        for a in Enemy:
-            a.hideturtle()
-        pen.clear()
-        break
+            pen.write("Score: {}".format(player.score), False, align="center", font=("Arial", 24, "normal"))
+            break
 
-window.mainloop()    
+player.hideturtle()
+for missile in missiles:
+    missile.hideturtle()
+for chattan in chattans:
+    chattan.hideturtle()
+pen.clear()
+
+window.mainloop()
